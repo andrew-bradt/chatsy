2277cd3ac361570727243b2538c03f2d430f173b
Andrew Bradt <andrewjbradt@gmail.com>
2022-04-14 18:56:57 -0400

    remove public dir and routes from boilerplate

--- server/app.js
+++ server/app.js
@@ -5,18 +5,11 @@ const logger = require('morgan');
 
 const db = require('./configs/db.config');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-
 const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter(db));
 
 module.exports = app;
