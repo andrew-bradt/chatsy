6c4b25a1d0a004f6126dbecf1267d44462472ba5
Roylu7384 <roylu7384@gmail.com>
2022-04-15 11:08:30 -0400

    Setup routes files

--- server/app.js
+++ server/app.js
@@ -3,6 +3,9 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const users = require("./routes/users");
+const contactInfo = require("./routes/contact-info");
+
 const db = require('./configs/db.config');
 
 const app = express();
@@ -11,5 +14,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use("/api/users", users(db));
+app.use("/api/contact-info", contactInfo(db));
 
 module.exports = app;
