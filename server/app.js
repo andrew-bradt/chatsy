8060020e324d5ea3c2eb45769e689bd68871ff93
Andrew Bradt <andrewjbradt@gmail.com>
2022-04-15 18:57:21 -0400

    remove io.connection

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const {v4: uuidv4} = require('uuid');
 
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -11,32 +12,17 @@ const db = require('./configs/db.config');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
-
-app.use(cors());
-
-const users = require("./routes/users");
-const contactInfo = require("./routes/contact-info");
-
-// socket io
-const {v4: uuidv4} = require('uuid');
-
-const userIDs = {};
-
-io.on('connection', (socket) => {
-  const userID = uuidv4();
-  userIDs[userID] = null;
-  socket.send(`Your id is ${userID}`);
+const io = socketio(server, {
+  cors: {
+    origin: '*'
+  }
 });
 
-// middleware, routes
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-app.use("/api/users", users(db));
-app.use("/api/contact-info", contactInfo(db));
 
 module.exports = {app, server};
