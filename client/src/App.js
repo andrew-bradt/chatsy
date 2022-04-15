0fcc4413f5fccb9d71deb88362e91484724bfbef
Andrew Bradt <andrewjbradt@gmail.com>
2022-04-15 18:17:09 -0400

    retry last commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import {io} from 'socket.io-client';
 
-const socket = io('http://localhost:8080');
+const socket = io('http://localhost:8080', { transports: ['websocket', 'polling', 'flashsocket'] });
+socket.on('message', (data) => {
+  console.log(data);
+});
 
 function App() {
   return (
