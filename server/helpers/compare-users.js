e369fdd229f53b459c103725bf751f4d959e2dbe
Andrew Bradt <andrewjbradt@gmail.com>
2022-04-16 18:29:39 -0400

    implement findCompatibleUsers

--- server/helpers/compare-users.js
+++ server/helpers/compare-users.js
@@ -1,14 +1,34 @@
 module.exports = (users) => {
+  const addCompatibleUser = (userOne, userTwo, interests) => {
+    userOne.compatibleUsers[userTwo.userId] = {
+      userId: userTwo.userId,
+      interests
+    };
+
+    userTwo.compatibleUsers[userOne.userId] = {
+      userId: userTwo,
+      interests
+    };
+  };
+
   const getOtherUserIds = (user) => {
-    Object.keys(users)
+    return Object.keys(users)
     .map(id => Number(id))
     .filter(id => id !== user.userId);
   };
     
   const findCompatibleUsers = (user) => {
-    console.log('TODO findCompatibleUsers');
     const userIds = getOtherUserIds(user);
-    
+
+    for (const userId of userIds) {
+      const commonInterests = [];
+      
+      users[userId].interests.forEach(interest => {
+        user.interests.has(interest) && commonInterests.push(interest);
+      });
+
+      commonInterests.length > 0 && addCompatibleUser(user, users[userId], commonInterests);
+    }
   };
 
   const removeCompatibleUser = (user) => {
