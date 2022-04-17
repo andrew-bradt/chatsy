52ea04f30cba0681ff80736fa25c328927327e3a
Andrew Bradt <andrewjbradt@gmail.com>
2022-04-17 11:18:45 -0400

    add Call class

---
+++ server/entities/Call.js
@@ -0,0 +1,20 @@
+class Call {
+  constructor(users) {
+    this.userIds = users.map(user => user.userId);
+    this.sharedInterests = this._getSharedInterests(users);
+  }
+
+  _getSharedInterests(users) {
+    const sharedInterests = [];
+    const [user1, user2] = users;
+    
+    user1.interests.forEach(interest => {
+      user2.interests.has(interest) && sharedInterests.push(interest);
+    })
+
+    return sharedInterests;
+  }
+
+}
+
+module.exports = Call;
