d3a1a86ec1b514cfad7b0739efcc9cf2f7555363
Andrew Bradt <andrewjbradt@gmail.com>
2022-04-20 16:09:33 -0400

    create modesMap which maps conditionals to determine the successive mode

--- client/src/hooks/useMode.js
+++ client/src/hooks/useMode.js
@@ -1,13 +1,22 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 const SIGNED_OUT = 'SIGNED_OUT';
 const OUTSIDE_LOBBY = 'OUTSIDE_LOBBY';
 const IN_LOBBY = 'IN_LOBBY';
 const IN_CALL = 'IN_CALL';
 
-export default function useMode({userId, remoteSocketId}) {
+
+export default function useMode({userId, remoteSocketId, inLobby}) {
   const [mode, setMode] = useState(SIGNED_OUT);
-  
+
+  useEffect(() => {}, [userId, remoteSocketId, inLobby]);
+
+  const modesMap = new Map([
+    [!userId, SIGNED_OUT],
+    [userId && !inLobby, OUTSIDE_LOBBY],
+    [userId && inLobby, IN_LOBBY],
+    [remoteSocketId && !inLobby, IN_CALL]
+  ]);
 
   return mode;
 };
