1ce6b124b7bf8b0d13489abc4d28347337629fd2
Andrew Bradt <andrewjbradt@gmail.com>
2022-04-20 19:32:03 -0400

    fixed useMode - successfully switches to IN_CALL from IN_LOBBY and vice versa

--- client/src/hooks/useMode.js
+++ client/src/hooks/useMode.js
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react';
+import {useRef, useEffect} from 'react';
 
 const SIGNED_OUT = 'SIGNED_OUT';
 const OUTSIDE_LOBBY = 'OUTSIDE_LOBBY';
@@ -6,21 +6,20 @@ const IN_LOBBY = 'IN_LOBBY';
 const IN_CALL = 'IN_CALL';
 
 export default function useMode({userId, remoteSocketId, inLobby}) {
-  
   const mode = (() => {
-  switch(true) {
-    case !userId:
-      return SIGNED_OUT;
-    case userId && !inLobby:
-      return OUTSIDE_LOBBY;
-    case inLobby && !remoteSocketId:
-      return IN_LOBBY;
-    case remoteSocketId:
-      return IN_CALL;
-    default:
-      break;
-    }
-  })();
-
+    switch(true) {
+      case !userId:
+        return SIGNED_OUT;
+      case userId && !inLobby:
+        return OUTSIDE_LOBBY;
+      case inLobby && !remoteSocketId:
+        return IN_LOBBY;
+      case !(!remoteSocketId):
+        return IN_CALL;
+      default:
+        break;
+      }
+    })();
+  
   return {mode};
 };
