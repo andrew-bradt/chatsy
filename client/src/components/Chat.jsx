d5c65d77536a75284d3e49ead7fa5ac49da1e319
Andrew Bradt <andrewjbradt@gmail.com>
2022-04-20 12:11:48 -0400

    create send method

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -298,40 +298,49 @@ const testMessages = [
   }
 ];
 
-const styles = {
-  wrapper: css({
-    backgroundColor: 'pink',
-    width: '100%',
-    height: '75%'
-  }),
 
-  child: css({
-    width: '100%',
-    backgroundColor: 'green'
-  }),
-    
-  msgBox: css({
-    height: '90%',
-    display: 'flex',
-    flexDirection: 'column',
-    border: '3px solid black',
-    overflowY: 'scroll'
-  })
-};
 
 export default function Chat({socket}) {
   const [messages, setMessages] = useState(testMessages);
   const [inputVal, setInputVal] = useState();
-
+  
+  const send = (e) => {
+    e.preventDefault();
+    console.log('send msg');
+  };
 
   return (
-    <Box css={styles.wrapper}>
-      <Box css={styles.msgBox}>
+    <Box css={wrapper}>
+      <Box 
+        css={msgBox}
+      >
         {messages.map((message, i) => <ChatListItem key = {i} text = {message.text} fromPeer = {message.fromPeer}/>)}
       </Box>
-      <Box component='form'>
-        <TextField id='compose-msg'/>
+      <Box component='form' onSubmit = {(e) => send(e)}>
+        <TextField css={textField} id='compose-msg'/>
       </Box>
     </Box>
   );
 };
+
+const wrapper = css({
+  backgroundColor: 'pink',
+  width: '100%',
+  height: '75%'
+});
+
+const wrapperChildren = css({
+  overflowY: 'scroll',
+  width: '100%'
+});
+
+const msgBox = css(wrapperChildren, {
+  height: '90%',
+  display: 'flex',
+  flexDirection: 'column',
+  border: '3px solid black'
+});
+
+const textField = css(wrapperChildren, {
+
+});
