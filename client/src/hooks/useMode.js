7fbc56f6c17205f97604e7a59acd4b68181b5b8b
Andrew Bradt <andrewjbradt@gmail.com>
2022-04-20 19:20:11 -0400

    remove useState from useMode

--- client/src/hooks/useMode.js
+++ client/src/hooks/useMode.js
@@ -6,33 +6,21 @@ const IN_LOBBY = 'IN_LOBBY';
 const IN_CALL = 'IN_CALL';
 
 export default function useMode({userId, remoteSocketId, inLobby}) {
-  const [mode, setMode] = useState(SIGNED_OUT);
+  
+  const mode = (() => {
+  switch(true) {
+    case !userId:
+      return SIGNED_OUT;
+    case userId && !inLobby:
+      return OUTSIDE_LOBBY;
+    case inLobby && !remoteSocketId:
+      return IN_LOBBY;
+    case remoteSocketId:
+      return IN_CALL;
+    default:
+      break;
+    }
+  })();
 
-  // const newMode = (() => {
-  // switch(true) {
-  //   case !userId:
-  //     return SIGNED_OUT;
-  //   case userId && !inLobby:
-  //     return OUTSIDE_LOBBY;
-  //   case inLobby && !remoteSocketId:
-  //     return IN_LOBBY;
-  //   case remoteSocketId:
-  //     return IN_CALL;
-  //   default:
-  //     break;
-  //   }
-  // })();
-  const changeMode = (newMode) => (mode !== newMode) && setMode(newMode);
-
-  if(!userId) {
-    changeMode(SIGNED_OUT);
-  } else if (userId && !inLobby) {
-    changeMode(OUTSIDE_LOBBY);
-  } else if (inLobby && !remoteSocketId) {
-    changeMode(IN_LOBBY);
-  } else if (remoteSocketId) {
-    changeMode(IN_CALL);
-  }
-  console.log(mode);
   return {mode};
 };
