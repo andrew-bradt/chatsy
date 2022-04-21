b6394b37b5fd561cdc4354eb07d73173b8542da5
Roylu7384 <roylu7384@gmail.com>
2022-04-21 09:15:45 -0400

    Add Video component

---
+++ client/src/components/Video.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Container } from "@mui/material";
+
+export default function Video() {
+  
+  return (
+      <img src="https://source.unsplash.com/random/640x480" alt="placeholder"></img>
+  )
+}
