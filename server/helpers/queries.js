18211aaf2daca2123f26a9105443a8b1ec75ecfd
Andrew Bradt <andrewjbradt@gmail.com>
2022-04-21 20:06:50 -0400

    add getUserTags

--- server/helpers/queries.js
+++ server/helpers/queries.js
@@ -25,5 +25,20 @@ module.exports = (db) => ({
       .then(res => {
         return res.rows;
     });
+  },
+
+  getUserTags(email) {
+    const queryString = `
+      SELECT users.id, email, label
+      FROM users
+      JOIN users_tags ON users.id = users_tags.user_id
+      JOIN tags ON tag_id = tags.id
+      WHERE email = $1
+    `;
+    const queryParams = [email];
+    return db.query(queryString, queryParams)
+      .then(res => {
+        return res.rows;
+    });
   }
 });
