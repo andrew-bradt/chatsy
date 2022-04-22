8b35a0ffebd0408ec8cdd5b4be5cb85e168e1c5e
Roylu7384 <roylu7384@gmail.com>
2022-04-22 13:27:26 -0400

    Validate user

--- server/youtube-queries/check-user.js
+++ server/youtube-queries/check-user.js
@@ -1,19 +1,26 @@
 const { google } = require('googleapis');
 
-const checkGoogleUser = async (auth, db) => {
-  
-  const {getUserId} = require('../helpers/queries')(db);
+const checkGoogleUser = async(auth, db) => {
+  // import db related queries methods 
+  const {getUserId, addUser} = require('../helpers/queries')(db);
 
+  // call oauth2 api for user email
   const o2 = google.oauth2({
     auth: auth,
     version: "v2"
   });
-
   const response = await o2.userinfo.get({});
   const userEmail = response.data.email;
 
+  // check if user exist in db
   const userId = await getUserId(userEmail);
-  console.log(userId);
+  if (userId) {
+    return userEmail;
+  }
+
+  // if user is new, add to db
+  const dbRes = await addUser(userEmail);
+  return dbRes.rows[0].email;
 
 };
 
