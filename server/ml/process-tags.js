07eb3534d9f027db1b6fee18ba7aeeefdbb91a4d
Andrew Bradt <andrewjbradt@gmail.com>
2022-04-23 11:19:25 -0400

    import needed packages and make template functions in process-tags

--- server/ml/process-tags.js
+++ server/ml/process-tags.js
@@ -1,11 +1,29 @@
 // TODO:
-// import axios, form data
-// get API Key from process.env
-// define parseLabel
-// define parseArray
 // ml function
   // accepts array of tags
   // parse to string
   // make api call
   // extract labels 
   // return parsed labels
+
+const axios = require('axios');
+const FormData = require('form-data');
+const tags = require('../../LOCAL/mock-data');
+
+const parseLabel = (label) => {
+
+};
+
+const parseTags = () => {
+
+};
+
+const API_KEY = process.env.MEANING_CLOUD_API;
+
+const processTags = (tags) => {
+  
+};
+
+processTags(tags);
+
+module.exports = processTags;
