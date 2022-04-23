e7da834cadf59bc0e8d32ae29286be6c3b7ea3b9
Andrew Bradt <andrewjbradt@gmail.com>
2022-04-23 11:22:21 -0400

    implement parsetags

--- server/ml/process-tags.js
+++ server/ml/process-tags.js
@@ -10,18 +10,17 @@ const axios = require('axios');
 const FormData = require('form-data');
 const tags = require('../../LOCAL/mock-data');
 
+const parseTags = (tags) => tags.join(', ');
+
 const parseLabel = (label) => {
-
-};
-
-const parseTags = () => {
-
+  
 };
 
 const API_KEY = process.env.MEANING_CLOUD_API;
 
 const processTags = (tags) => {
-  
+  const parsedTags = parseTags(tags);
+  console.log(parsedTags);
 };
 
 processTags(tags);
