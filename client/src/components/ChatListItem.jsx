16f8ed9b58167085d042bff067f29b69e12ed5f4
Andrew Bradt <andrewjbradt@gmail.com>
2022-04-25 14:12:50 -0400

    add functions to conditionally add padding based on text length

--- client/src/components/ChatListItem.jsx
+++ client/src/components/ChatListItem.jsx
@@ -4,27 +4,38 @@ import React from 'react'
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 
+const calcPaddingByStringLength = ({paddingShort, paddingLong, lengthThresh}) => {
+  return (string) => {
+    return (string.length < lengthThresh) ? paddingShort : paddingLong;
+  }
+};
+const calcRightPadding = calcPaddingByStringLength({paddingShort: '2.75%', paddingLong: '2%', lengthThresh: 50});
+const calcLeftPadding = calcPaddingByStringLength({paddingShort: '2.25%', paddingLong: '2%', lengthThresh: 50});
+
 const message = css({
-  width: '66%',
+  maxWidth: '66%',
   padding: '2%',
-  margin: '2%'
+  marginBottom: '2%',
+  borderRadius: '15px',
 });
 
-const peerMessage = css(message, {
-  borderRadius: '0px 5px 5px 5px',
+const peerMessage = (text) => css(message, {
+  borderTopLeftRadius: 0,
+  alignSelf: 'flex-start',
+  paddingRight: calcRightPadding(text)
 });
 
-const clientMessage = css(message, {
-  borderRadius: '5px 0px 5px 5px',
+const clientMessage = (text) => css(message, {
+  borderTopRightRadius: 0,
   alignSelf: 'flex-end',
-  marginRight: '4%'
+  paddingLeft: calcLeftPadding(text)
 });
 
 export default function ChatListItem({text, fromPeer}) {
   return (
     <Paper
-      elevation={3}
-      css = {(fromPeer) ? peerMessage : clientMessage}
+      elevation={1}
+      css = {(fromPeer) ? peerMessage(text) : clientMessage(text)}
     >
       <Typography>{text}</Typography>
     </Paper>
