73a6a2463c6b283b11e25ebef164690b741fd704
Andrew Bradt <andrewjbradt@gmail.com>
2022-04-25 12:59:10 -0400

    flip x-axis for elements in video component

--- client/src/components/Video.jsx
+++ client/src/components/Video.jsx
@@ -9,7 +9,8 @@ export default function Video(props) {
   const children = css({
     borderRadius: '10px',
     width: '50%',
-    boxShadow: '0px 3px 3px -2px rgb(0 0 0 / 20%), 0px 3px 4px 0px rgb(0 0 0 / 14%), 0px 1px 8px 0px rgb(0 0 0 / 12%)'
+    boxShadow: '0px 3px 3px -2px rgb(0 0 0 / 20%), 0px 3px 4px 0px rgb(0 0 0 / 14%), 0px 1px 8px 0px rgb(0 0 0 / 12%)',
+    transform: 'scaleX(-1)'
   });
 
   return (
