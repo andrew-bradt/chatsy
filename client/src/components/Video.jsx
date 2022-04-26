ea1f641ac0ab88986a15abb88397161e0861ebf5
Andrew Bradt <andrewjbradt@gmail.com>
2022-04-26 08:35:46 -0400

    Add shortcircuit to adding client video to DOM

--- client/src/components/Video.jsx
+++ client/src/components/Video.jsx
@@ -7,7 +7,7 @@ export default function Video(props) {
   
   return (
     <>
-        <video ref={videoRef} autoPlay css={clientVideo}></video>
+      {videoRef && <video ref={videoRef} autoPlay css={clientVideo}></video>}
       {!remoteSocketId && <img src="https://cdn.dribbble.com/users/563824/screenshots/3633228/media/d876c7712d969c0656302b16b16af2cc.gif" alt="placeholder" css={peerVideo}></img>}
       {remoteSocketId && <video ref={remoteVideoRef} autoPlay css={peerVideo}></video>}
       </>
